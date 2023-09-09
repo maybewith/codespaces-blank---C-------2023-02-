724bea5133e57f1e39f07ec5e869423e6aa5722c
GwangGay <97596488+maybewith@users.noreply.github.com>
2023-09-09 06:39:36 +0000

    2023-09-09

--- 9장 포인터 실습 - 문제 8.c
+++ 9장 포인터 실습 - 문제 8.c
@@ -2,20 +2,32 @@
 
 // 두 정수의 값을 교환하는 함수
 void swap(int *a, int *b) {
+    // 임의의 변수를 활용하여 값을 교환시킨다
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
 int main() {
+    // 필요한 변수와 배열을 선언을 한다
     int N, a, b;
     int arr[50];
+
+    // 개수를 입력을 받는다
     scanf("%d", &N);
+
+    // 반복문을 통해서 배열에 필요한 것들을 입력을 받는다
     for (int i = 0; i < N; ++i) {
         scanf("%d", &arr[i]);
     }
+
+    // 추가로 입력을 받는다
     scanf("%d %d", &a, &b);
+
+    // 변수를 바꿔준다
     swap(&arr[a], &arr[b]);
+
+    // 반복문을 통해서 출력을 한다
     for (int i = 0; i < N; ++i) {
         printf("%d ", arr[i]);
     }
--- 9장 포인터 실습 - 문제 9.c
+++ 9장 포인터 실습 - 문제 9.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 
 // 중앙값을 찾아주는 함수
-int* mid(int arr[]) {
+int* mid(int *arr) {
+    // 저장할 변수를 선언한다
     int *mid_val;
-    if ((arr[0] >= arr[1] && arr[0] <= arr[2]) || (arr[0] >= arr[2] && arr[0] <= arr[1])) {
-        mid_val = &arr[0];
-    } else if ((arr[1] >= arr[0] && arr[1] <= arr[2]) || (arr[1] >= arr[2] && arr[1] <= arr[0])) {
-        mid_val = &arr[1];
+
+    // 만약 첫번째거라면, 두번째라면, 세번째라면.....
+    if ((*arr >= *(arr+1) && *arr <= *(arr+2)) || (*arr >= *(arr+2) && *arr <= *(arr+1))) {
+        mid_val = arr;
+    } else if ((*(arr+1) >= *arr && *(arr+1) <= *(arr+2)) || (*(arr+1) >= *(arr+2) && *(arr+1) <= *arr)) {
+        mid_val = arr+1;
     } else {
-        mid_val = &arr[2];
+        mid_val = arr+2;
     }
+
+    // 값을 반환을 해준다
     return mid_val;
 }
 
 int main() {
+    // 필요한 포인터와 배열을 선언한다
+    int *p;
     int arr[3];
-    for (int i = 0; i < 3; ++i) {
-        scanf("%d", &arr[i]);
+
+    // 입력을 받는다
+    for (p = arr; p < arr + 3; p++) {
+        scanf("%d", p);
     }
+
+    // 중앙값을 찾아주는 함수에 배열을 넣는다 
     int *mid_value = mid(arr);
+
+    // 값을 출력을 해준다
     printf("%d\n", *mid_value);
+
     return 0;
 }
