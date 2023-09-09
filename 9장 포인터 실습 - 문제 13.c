7a592082b708cf01b0d6adcb06d5bcc587ff7b2e
GwangGay <97596488+maybewith@users.noreply.github.com>
2023-09-09 07:04:44 +0000

    2023-09-09

--- 9장 포인터 실습 - 문제 13.c
+++ 9장 포인터 실습 - 문제 13.c
@@ -1,29 +1,38 @@
 #include <stdio.h>
 
+// 배열의 합을 구하는 함수
 int arrsum(int *start, int *end) {
+    // 총 합을 기억할 변수
     int sum = 0;
+
+    // 반복문을 통해서 총합을 구한다
     for (int *p = start; p <= end; ++p) {
         sum += *p;
     }
+
+    // 결과를 반환한다
     return sum;
 }
 
 int main() {
+    // 필요한 변수를 선언한다
     int N, S, E;
+
+    // 입력들을 받는다
     scanf("%d", &N);
     scanf("%d %d", &S, &E);
 
+    // 배열을 선언하고 입력을 받는다
     int arr[100];
     for (int i = 0; i < N; ++i) {
         scanf("%d", arr + i);
     }
 
-    
-
-    
+    // 배열의 합을 함수에 넣어서 구해서 저장을 시킨다    
     int sum = arrsum(arr + S, arr + E);
+
+    // 결과를 출력을 해준다
     printf("%d\n", sum);
     
-
     return 0;
 }
--- 9장 포인터 실습 - 문제 14.c
+++ 9장 포인터 실습 - 문제 14.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
 
+// 첫 번째 포인터가 가리키는 배열의 원소부터 두 번째 포인터가 가리키는 배열의 원소까지, 
+// 배열의 원소 값을 더한 합을 계산하는 함수
 int add_to_k(int *start, int *end) {
     int sum = 0;
     int *p;
     for (p = start; p <= end; p++) {
         sum += *p;
     }
+
+    // 결과를 반환한다
     return sum;
 }
 
 int main() {
+    // 필요한 변수를 선언하고 입력을 받는다
     int N;
     scanf("%d", &N);
 
+    // 배열과 포인터를 선언을 해준다
     int d[100];
     int *p;
 
+    // 배열의 입력을 반복문을 통해서 입력 받는다
     for (p = d; p < d + N; p++) {
         scanf("%d", p);
     }
 
+    // 합을 구하기 위해서 반복문과 함수를 사용한다
     int sum = 0;
     for (p = d; p < d + N; p++) {
         sum += add_to_k(d, p);
     }
 
+    // 결과를 출력을 한다
     printf("%d\n", sum);
+
     return 0;
 }
