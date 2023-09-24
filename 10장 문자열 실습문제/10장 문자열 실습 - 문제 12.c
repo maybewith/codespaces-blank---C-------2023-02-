68c20d18ceaab13aa9378daa8e60f5e3e70a50b8
GwangGay <97596488+maybewith@users.noreply.github.com>
2023-09-24 16:06:14 +0000

    2023-09-25

--- 10장 문자열 실습문제/10장 문자열 실습 - 문제 12.c
+++ 10장 문자열 실습문제/10장 문자열 실습 - 문제 12.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
+    // 공백을 포함하는 문자열 str1과 공백을 포함하지 않는 문자열 str2를
+    // 입력 받는다.
     char str1[101], str2[101];
     fgets(str1, sizeof(str1), stdin);
     fgets(str2, sizeof(str2), stdin);
 
-    // 줄바꿈 문자 제거
-    str1[strcspn(str1, "\n")] = 0;
-    str2[strcspn(str2, "\n")] = 0;
+    // 문자열 길이 계산 (strlen 함수처럼 동작)
+    int len1 = 0, len2 = 0;
+    while (str1[len1] && str1[len1] != '\n') len1++;
+    while (str2[len2] && str2[len2] != '\n') len2++;
 
-    int len1 = strlen(str1);
-    int len2 = strlen(str2);
-    
+    // 줄바꿈 문자 제거
+    if (str1[len1] == '\n') str1[len1] = '\0';
+    if (str2[len2] == '\n') str2[len2] = '\0';
+
+    // str2가 str1에 몇 번 나타나는지, 그 횟수 세어서 변수에 저장 
     int count = 0;
     for (int i = 0; i <= len1 - len2;) {
-        if (strncmp(str1 + i, str2, len2) == 0) {
+        int j;
+        for (j = 0; j < len2; j++) {
+            if (str1[i + j] != str2[j]) {
+                break;
+            }
+        }
+        
+        // 만약 같다면 다음과 같이 한다.
+        if (j == len2) {
             count++;
             i += len2;
         } else {
@@ -23,6 +35,7 @@ int main() {
         }
     }
     
+    // 결과를 출력을 한다.
     printf("%d\n", count);
     return 0;
 }
